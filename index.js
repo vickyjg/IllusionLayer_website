52be7147e05365f32eaa1703b377fbe2cbfc6cbc
Victoria Jiang <vickyy.jg@gmail.com>
2019-12-23 02:00:39 -0500

    Added dot navigation to artist page

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ https://codepen.io/matthewcain/pen/ZepbeR
 var btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
 });
 
 btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
 });
 
 /* end of Back to Top script */
@@ -43,38 +43,47 @@ console.log(slideWidth);
 
 // arrange slides next to each other
 const setSlidePosition = (slide, index) => {
-     slide.style.left = slideWidth * index + 'px';
+    slide.style.left = slideWidth * index + 'px';
 };
 slides.forEach(setSlidePosition);
-    
- 
+
+// moves slides 
+
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentSlide.classList.remove('currentSlide');
     targetSlide.classList.add('currentSlide');
 };
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('currentSlide');
+    targetDot.classList.add('currentSlide');
+};
+
+
 // left click
 previousButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.currentSlide');
-     
     const previousSlide = currentSlide.previousElementSibling;
-    
+    const currentDot = nav.querySelector('.currentSlide');
+    const prevDot = currentDot.previousElementSibling;
+
     moveToSlide(track, currentSlide, previousSlide);
-    
- 
+    updateDots(currentDot, prevDot);
+
 });
 
 
 // right click
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.currentSlide');
-     
     const nextSlide = currentSlide.nextElementSibling;
-    
+    const currentDot = nav.querySelector('.currentSlide');
+    const nextDot = currentDot.nextElementSibling;
+
     moveToSlide(track, currentSlide, nextSlide);
-    
- 
+    updateDots(currentDot, nextDot);
+
 });
 
 
@@ -83,14 +92,18 @@ nextButton.addEventListener('click', e => {
 
 nav.addEventListener('click', e => {
     // what indicator was clicked on
-    
+
     const targetDot = e.target.closest('button');
-    
-    if (targetDot) return;
-    
+
+    if (!targetDot) return;
+
     const currentSlide = track.querySelector('.currentSlide');
     const currentDot = nav.querySelector('.currentSlide');
-    
     const targetIndex = dots.findIndex(dot => dot === targetDot);
-    
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+
+
 });
